69aff802650b893a539bb727e6de8fa69d453c43
Phun <aaaphun@gmail.com>
2018-05-01 15:32:02 +0800

    tmp

--- data_1.py
+++ data_1.py
@@ -2,16 +2,18 @@ from keras.preprocessing.image import img_to_array, load_img, array_to_img,Image
 import numpy as np
 import os
 
+
 train_num = 85
 test_num = 80
 
-x_train = np.ndarray((train_num,128,128,3), dtype=np.uint8)
-y_train = np.ndarray((train_num,128,128,1), dtype=np.uint8)
+x_train = np.ndarray((train_num,512,512,3), dtype=np.uint8)
+y_train = np.ndarray((train_num,512,512,1), dtype=np.uint8)
 
-x_test = np.ndarray((test_num,128,128,1), dtype=np.uint8)
-y_test = np.ndarray((test_num,128,128,1), dtype=np.uint8)
+x_test = np.ndarray((test_num,512,512,1), dtype=np.uint8)
+y_test = np.ndarray((test_num,512,512,1), dtype=np.uint8)
 
 
+'''
 count = 0
 print('-'*30)
 print("loading training data...")
@@ -19,11 +21,10 @@ dir = "D:\dataset\data-train\img"
 for root, dirs, files in os.walk(dir):
     for file in files:
         name = os.path.join(root,file).split("D:\\dataset\\data-train\\img\\")[1].split(".")[0]
-        img = load_img(dir+"\\"+name+".bmp",target_size=(128,128))
-        label = load_img("D:\dataset\data-train\label\\"+name+"_anno.bmp",grayscale = True,target_size=(128,128))
+        img = load_img(dir+"\\"+name+".bmp",target_size=(512,512))
+        label = load_img("D:\dataset\data-train\label\\"+name+"_anno.bmp",grayscale = True,target_size=(512,512))
         img = img_to_array(img)
         label = img_to_array(label)
-        label[label != 0] = 255 # 二值化
         x_train[count] = img
         y_train[count] = label
         #print(x_train)
@@ -73,8 +74,10 @@ img_num = 0
 for num in os.listdir(dir): #fn 表示的是文件名
         img_num += 1
 
-new_img = np.ndarray((img_num,128,128,1), dtype=np.uint8)
-new_label = np.ndarray((img_num,128,128,1), dtype=np.uint8)
+
+new_img = np.ndarray((img_num,512,512,1), dtype=np.uint8)
+new_label = np.ndarray((img_num,512,512,1), dtype=np.uint8)
+
 
 count = 0
 for root, dirs, files in os.walk(dir):
@@ -86,10 +89,12 @@ for root, dirs, files in os.walk(dir):
         img = img_to_array(img)
         label = img[:,:,2]
         
-        label = label.reshape((128,128,1))
+        label = label.reshape((512,512,1))
         
         new_label[count] = label
+        label[label != 0] = 255
         label = array_to_img(label)
+        
         label.save("D:\dataset\data-augmentation\label\\"+str(count)+".bmp")
         
         img[:,:,2] = 0
@@ -103,7 +108,11 @@ for root, dirs, files in os.walk(dir):
         #print(new_img[count].shape)
         count += 1
 
+#np.save('x_train.npy',new_img)
+#np.save('y_train.npy',new_label)
+
 np.save('x_train.npy',new_img)
+
 np.save('y_train.npy',new_label)
 
 count = 0
@@ -112,15 +121,18 @@ dir = "D:\dataset\data-test\img"
 for root, dirs, files in os.walk(dir):
     for file in files:
         name = os.path.join(root,file).split("D:\\dataset\\data-test\\img\\")[1].split(".")[0]
-        img = load_img(dir+"\\"+name+".bmp",grayscale = True,target_size=(128,128))
-        label = load_img("D:\dataset\data-test\label\\"+name+"_anno.bmp",grayscale = True,target_size=(128,128))
+        img = load_img(dir+"\\"+name+".bmp",grayscale = True,target_size=(512,512))
+        label = load_img("D:\dataset\data-test\label\\"+name+"_anno.bmp",grayscale = True,target_size=(512,512))
         img = img_to_array(img)
         label = img_to_array(label)
         label[label != 0] = 255
         x_test[count] = img
         y_test[count] = label
         count += 1
+        
 np.save('x_test.npy',x_test)
+
 np.save('y_test.npy',y_test)
+
 print('complete')
 print('-'*30)
